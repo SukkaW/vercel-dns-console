7f09aeeb3ea2ecc8220a10b9ffc19e15b03fb65c
SukkaW <isukkaw@gmail.com>
2025-11-20 22:39:29 +0800

    chore: housekeeping

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,19 +1,19 @@
 export const isBrowser = typeof window !== 'undefined';
 
-export const formatDate = (ts: number): string => {
+export function formatDate(ts: number): string {
   const offset = (new Date()).getTimezoneOffset();
   const date = new Date(ts + offset * 60 * 1000);
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-};
+}
 
-export const clamp = (number: number, lower: number | undefined, upper: number | undefined) => {
-  if (number === number) {
-    if (upper != null) {
-      number = number <= upper ? number : upper;
-    }
-    if (lower != null) {
-      number = number >= lower ? number : lower;
-    }
-  }
-  return number;
-};
+// export const clamp = (number: number, lower: number | undefined, upper: number | undefined) => {
+//   if (number === number) {
+//     if (upper != null) {
+//       number = number <= upper ? number : upper;
+//     }
+//     if (lower != null) {
+//       number = number >= lower ? number : lower;
+//     }
+//   }
+//   return number;
+// };
